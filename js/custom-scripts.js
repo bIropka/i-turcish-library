e7ab560e610499f7e50a58d73a1d88b84e6ae522
Iegor Stetsiura <biropka@gmail.com>
2016-07-07 02:44:27 +0300

    Initial commit

---
+++ js/custom-scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    /** nav-menu script **/
+    $('.nav-icon').click(function() {
+        $('nav ul li').slideToggle();
+    });
+    /** the end of the nav-menu script **/
+
+});
