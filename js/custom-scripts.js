2f580265db2bb21a50297eb9cd5436ab20574207
Iegor Stetsiura <biropka@gmail.com>
2016-07-08 01:32:36 +0300

    v 0.8

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
 
     /** nav-menu script **/
+
     $('.nav-icon').click(function() {
         $('nav ul li').slideToggle();
     });
+
     /** the end of the nav-menu script **/
+
+    /** show/hide courses **/
+
+    $('.courses>button').click(function() {
+        $(this).toggleClass('active');
+        $(this).find('span').toggleClass('visible');
+        $(this).siblings('ul').slideToggle(400);
+    });
+
+    /** end of script for courses**/
 
 });
